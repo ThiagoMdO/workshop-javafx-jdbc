f4f2744f526c9c5366c61e7a5d9904fada0d42d0
ThiagoMdO <thiago_catureba@hotmail.com>
2024-03-20 16:17:31 -0300

    Main view controller

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -6,3 +6,7 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 p
 @src/main/java/com/corse/workshopjavafxjdbc/HelloApplication.java,6\d\6dfadcfe6a43e6b28e57c62c3df261ad06224461
+o
+?src/main/java/com/corse/workshopjavafxjdbc/MainApplication.java,0\4\046924efc0f66afbcd64cdd73ff2d7cd2eeabf09
+n
+>src/main/java/com/corse/workshopjavafxjdbc/MainController.java,8\f\8ffd882efa11881fdc83d44a66bac460efaee96c
--- src/main/java/com/corse/workshopjavafxjdbc/MainApplication.java
+++ src/main/java/com/corse/workshopjavafxjdbc/MainApplication.java
@@ -8,11 +8,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main-view.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Main-view.fxml"));
 
             ScrollPane scrollPane = fxmlLoader.load();
 
