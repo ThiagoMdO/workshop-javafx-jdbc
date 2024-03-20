eb69e980bed77161424187b111b90b4dfc366913
ThiagoMdO <thiago_catureba@hotmail.com>
2024-03-20 17:55:29 -0300

    DepartmentList view design

--- src/main/java/com/corse/workshopjavafxjdbc/MainController.java
+++ src/main/java/com/corse/workshopjavafxjdbc/MainController.java
@@ -33,7 +33,7 @@ public class MainController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        System.out.println("onMenuItemDepartmentAction");
+        loadView("gui/DepartmentList.fxml");
     }
 
     @FXML
