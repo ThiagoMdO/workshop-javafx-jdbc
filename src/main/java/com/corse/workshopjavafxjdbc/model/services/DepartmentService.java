72ddc4b9d71b0f402acecb00c5cd248442771d37
ThiagoMdO <thiago_catureba@hotmail.com>
2024-03-22 09:28:09 -0300

    Saving a new Department

--- src/main/java/com/corse/workshopjavafxjdbc/model/services/DepartmentService.java
+++ src/main/java/com/corse/workshopjavafxjdbc/model/services/DepartmentService.java
@@ -11,7 +11,12 @@ public class DepartmentService {
 
     private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
-    public List<Department> findAll(){
+    public List<Department> findAll() {
         return dao.findAll();
+    }
+
+    public void saveOrUpdate(Department obj) {
+        if (obj.getId() == null) dao.insert(obj);
+        dao.update(obj);
     }
 }
